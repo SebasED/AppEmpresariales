aae96331ab2263bc7713de4a7e4087c191dbf755
SebasED <johns.estrada@udea.edu.co>
2020-10-03 11:58:19 -0500

    dojo.Net

---
+++ dojo .NET/dojo/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using dojo.Modelo;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace dojo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuariosController : ControllerBase
+    {
+        FirebaseAccount b = FirebaseAccount.Instance;
+
+        Usuario usuario= new Usuario();
+        
+        public UsuariosController()
+        {
+        }
+        [HttpGet]
+        public async Task<List<Usuario>> Get(){
+            return await b.GetUser();
+        }
+        [HttpPost]
+        public async Task<String> Post(Usuario user){
+            return await b.AddUser(user);
+        }
+
+        [HttpDelete]
+        public async Task<String> Delete([FromQuery]String id){
+            return await b.deleteUser(id);
+        }
+
+    }
+}
